7eb185b2ee84c53ed2bf08523308d66ad36f4ff4
Perlo1 <juancarlosmondejar6@gmail.com>
2023-12-13 12:38:36 +0100

    welcome.jsx

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -7,16 +7,12 @@ export function Welcome({ name, age }) {
     <div>
       <h2>Hello, <strong>{name}</strong>!</h2>
 
-      {/* Render Age component only if age is greater than 18 */}
       {age > 18 && <Age age={age} />}
 
-      {/* Render Age component only if age is present */}
       {age && <Age age={age} />}
 
-      {/* Render Age component only if age is between 18 and 65 (inclusive) */}
       {age > 18 && age < 65 && <Age age={age} />}
 
-      {/* Render Age component only if age is between 18 and 65, and name is "John" */}
       {age > 18 && age < 65 && name === 'John' && <Age age={age} />}
     </div>
   );
