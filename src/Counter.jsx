dfe2e76492393d3d7b06b6cc5f2dd6b3570cc037
Perlo1 <juancarlosmondejar6@gmail.com>
2023-12-14 18:03:47 +0100

    useEffect

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function CounterDisplay({ count }) {
   return <h2>I have counted to {count}</h2>;
@@ -6,6 +6,14 @@ function CounterDisplay({ count }) {
 
 export function Counter({ initialValue = 0, decrementAmount = 1 }) {
   const [counter, setCounter] = useState(initialValue);
+
+  useEffect(() => {
+       
+  }, []);
+
+  useEffect(() => {
+    console.log(`The value of the counter is ${counter}`);
+  }, [counter, initialValue]);
 
   function handleCounterIncrement() {
     setCounter((c) => c + 1);
